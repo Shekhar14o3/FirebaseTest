369974c8d96c91b9399a676ba0d4de476251f1bb
Shekhar14o3 <aman.shekhar4@gmail.com>
2017-01-15 00:30:30 +0530

    Latest

--- app/src/main/java/com/firebasetesting/MainActivity.java
+++ app/src/main/java/com/firebasetesting/MainActivity.java
@@ -1,7 +1,9 @@
 package com.firebasetesting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PagerTabStrip pagerTabStrip = (PagerTabStrip) findViewById(R.id.pager_header);
+
+        ((ViewPager.LayoutParams) pagerTabStrip.getLayoutParams()).isDecor = true;
     }
 }
